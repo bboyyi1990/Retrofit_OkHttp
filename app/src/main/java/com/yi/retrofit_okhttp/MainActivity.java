ad5439fd54b6d6ea8853eb329c968a973359caa8
Yi <Yi@YideMacBook-Pro.local>
2017-07-20 21:06:06 +0800

    okhttp practice - post request

--- app/src/main/java/com/yi/retrofit_okhttp/MainActivity.java
+++ app/src/main/java/com/yi/retrofit_okhttp/MainActivity.java
@@ -1,13 +1,86 @@
 package com.yi.retrofit_okhttp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.gxz.PagerSlidingTabStrip;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    PagerSlidingTabStrip slidingTabStrip;
+    private ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pager = (ViewPager) findViewById(R.id.pager);
+        slidingTabStrip = (PagerSlidingTabStrip) findViewById(R.id.tabs);
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add("TAB " + i);
+        }
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager(), list));
+        slidingTabStrip.setViewPager(pager);
+
+        slidingTabStrip.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+    }
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        private ArrayList<String> titles;
+
+
+        public MyPagerAdapter(FragmentManager fm, ArrayList<String> list) {
+            super(fm);
+            this.titles = list;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
+
+
+        @Override
+        public Fragment getItem(int position) {
+            Bundle b = new Bundle();
+            b.putString("title", titles.get(position));
+            return FragmentContent.getInstance(b);
+        }
+
+        @Override
+        public int getCount() {
+            return null == titles ? 0 : titles.size();
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            return super.instantiateItem(container, position);
+        }
+
+
     }
 }
--- okhttp/src/main/java/com/Yi/HelloOkHttp.java
+++ okhttp/src/main/java/com/Yi/HelloOkHttp.java
@@ -2,6 +2,11 @@ package com.Yi;
 
 import java.io.IOException;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Headers;
+import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -11,19 +16,102 @@ import okhttp3.Response;
  */
 
 public class HelloOkHttp {
-    public static void main(String args[]) {
 
+    /**
+     * 同步网络请求
+     */
+    public static void sendReqeust() {
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().
                 url("http://www.imooc.com").build();
         try {
             Response response = client.newCall(request).execute();
-            if (response.isSuccessful()){
+            if (response.isSuccessful()) {
                 System.out.print(response.body().string());
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public static void sendAsyncRequest(String url) {
+        System.out.print("Current Thread ->" + Thread.currentThread().getName());
+        OkHttpClient okHttpClient = new OkHttpClient();
+        Request request = new Request.Builder().url(url)
+                .addHeader("User-Agent", "from Yi http") //加入请求头
+                .addHeader("Accept", "text/plain,text/html")
+                .build();
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    System.out.print("Response Thread ->" + Thread.currentThread().getName());
+//                    System.out.print(response.body().string());
+                    for (int i = 0; i < response.headers().size(); i++) {
+                        System.out.print(response.headers().name(i) + " : " + response.headers().value(i));
+                    }
+                }
+            }
+        });
+    }
+
+    public static void queryHttp() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        HttpUrl httpUrl = HttpUrl.parse("https://api.heweather.com/x3/weather").
+                newBuilder().
+                addQueryParameter("city", "beijing").
+                addQueryParameter("key", "").
+                build();
+        Request request = new Request.Builder().url(httpUrl).build();
+        String url = httpUrl.toString();
+        System.out.print(url);
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    System.out.print(response.body().string());
+                }
+            }
+        });
+    }
+
+    public static void postRequest() {
+        OkHttpClient okHttpClient = new OkHttpClient();//OKHttp 客户端
+
+        FormBody body = new FormBody.Builder().add("userName", "Yi").add("userAge", "27").build();//post请求体
+
+        Request request = new Request.Builder().url("").post(body).build(); //请求对象
+
+        //客户端请求回调
+        okHttpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    System.out.print(response.body().string());
+                }
+            }
+        });
+    }
+
+    public static void main(String args[]) {
+//        sendReqeust();
+//        sendAsyncRequest("http://www.imooc.com");
+//        queryHttp();
+        postRequest();
     }
 }
